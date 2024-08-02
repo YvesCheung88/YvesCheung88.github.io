6f49af793194a6158bd4f18199159fcdfefad4ab
YvesCheung88 <yves.cheung.88@gmail.com>
2024-08-02 10:54:43 +0200

    nav bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<nav>
+	<ul class="header-menu">
+		<li><a href="/">Blog</a></li>
+		<li><a href="/">Portfolio</a></li>
+		<li><a href="/">Contact</a></li>
+		<li><a href="/">CV</a></li>
+	</ul>
+</nav>
+<slot></slot>
+
+<style>
+	nav {
+		margin-top: 30px;
+	}
+	.header-menu {
+		display: flex;
+		list-style: none;
+		gap: 10px;
+		font-size: 32px;
+		font-weight: bold;
+
+		& a {
+			text-decoration: none;
+			cursor: pointer;
+			color: black;
+			padding: 5px;
+		}
+	}
+	.header-menu a:hover {
+		background-color: blue;
+	}
+</style>
