4c0fd3050bde4d536adf2ea8ba3d2bcff3b27cfe
YvesCheung88 <yves.cheung.88@gmail.com>
2024-08-19 17:12:37 +0200

    wakatime add stat

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<div>
+	<figure>
+		<embed
+			src="https://wakatime.com/share/@2abe4981-4993-4ede-afc7-0d7031c60e7a/9620a1ba-e9a4-48c2-bcd5-8b1ef103e071.svg"
+		/>
+	</figure>
+
+	<figure>
+		<embed
+			src="https://wakatime.com/share/@2abe4981-4993-4ede-afc7-0d7031c60e7a/e603372d-49d5-4a21-ac01-7da5d08abe2d.svg"
+		/>
+	</figure>
+</div>
+
+<style>
+	div {
+		width: 800px;
+		margin: auto;
+		display: grid;
+		grid-template-rows: auto auto;
+		gap: 2rem;
+		& figure {
+			display: grid;
+			place-items: center;
+		}
+	}
+</style>
